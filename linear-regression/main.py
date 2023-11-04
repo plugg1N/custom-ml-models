593e5c1889283f6c876dfb6ab38d8db1919298e2
plugg1N <bbulkaskoricey87@gmail.com>
2023-11-04 10:03:23 +0300

    finish simplest version

--- linear-regression/main.py
+++ linear-regression/main.py
@@ -1,52 +1,65 @@
+"""
+Simplest linear regression model without:
+
+- Gradient Descent
+- Multi-layer input
+
+
+Sometimes cost can go up, due to the
+fact that weights and bias become bigger than needed
+
+Best params:
+cost = 0.00503168492059078, w = 1.97851323163009, b = 0.13165922797451451
+
+"""
+
+
+
 import random
+import numpy as np
 
 # Data
 X = [i for i in range(10)]
 y = [i*2 for i in X]
 
 # Constants
-b0 = 0                      # bias
-alpha = 1e-1                # learning rate
-eps = 1e-1                  # epsilon
-w = random.randint(0, 20)   # random weight for init
+b0 = random.randint(1, 20)  # bias
+alpha = 1e-3                # learning rate
+eps = 1e-3                  # epsilon
+w = random.randint(1, 20)   # random weight
 
 
-# Calculate the function
-def h(x: int, w: float, b0: int) -> int:
-    return (x * w) + b0
+
+# Calculate predictions based on Linear Regression method
+def h(w, b0) -> list:
+    return np.array(X).dot(w) + b0
 
 
-# Calculate cost function
-def cost(y_true: list, y_preds: list) -> float:
-    sums = 0
-    for i in range(len(y_true)):
-        sums += (y_true[i] - y_preds[i]) ** 2
-    return sums / len(y_true)
-
-
-"""
-# Gradient descent
-def gradient_descent(X: list, y: list, w: int):
-    preds = []
-    for i in range(len(X)):
-        preds.append( h(X[i], w, b0) )
-    J = cost(y, preds)
-
+# Calculate MSE
+def cost(w, b0) -> float:
+    result = 0
     
-    while J > 0:
-        preds = []
+    for i in range(len(X)):
+        result += (y[i] - h(w, b0)[i]) ** 2
 
-        abomination = alpha * ( (h(X[i]+eps, w, b0) + h(X[i], w, b0)) / eps )
-        tmp = w - abomination
-        w = tmp
+    return result / len(X)
 
-        for j in range(len(X)):
-            preds.append( h(X[j], w, b0) )
 
-        J = cost(y, preds)
-        print(J)
+# Tuning (!NOT GRADIENT DESCENT)
+for i in range(7_000):
+    
+    # Cost without tuning
+    c = cost(w, b0)
+
+    # Find derivatives for each weights and biases
+    dw = (cost(w + eps, b0) - c) / eps
+    db = (cost(w, b0 + eps) - c) / eps
+
+    # Tune parameters
+    w -= dw * alpha
+    b0 -= db * alpha
+
+    # Calculate cost and print
+    print(f"cost = {cost(w, b0)}, w = {w}, b = {b0}")
+
         
-    return w
-"""
-
-
